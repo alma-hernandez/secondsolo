8e40d81bb8937941184a390ba98f392bb30eef35
Alma Duffey <1984ahern@gmail.com>
2023-12-03 16:19:31 -0600

    added more pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,11 +8,12 @@ const Home = () => {
     return (
         <div>
             <ColorSchemesExample />
-
+           
             <h1>How We Met</h1>
 
             <img src={stateFairMural} alt="State Fair Mural" />
             <p id="intro">We used an app called "Hinge",  who's slogan is "The app designed to be deleted." They were 100% right! We met very quickly for lunch at Torchy's Tacos. We were having so much fun, we decided to go play indoor mini-golf. Unfortunately, the wait was 1.5hr, so we played some arcade games instead. He was a complete gentleman, and dropdead gorgeous. I couldn't wait to see him again. We both really wanted a family and had the same thoughts when it came to raising one. There was never a better match. Just a couple months later on November 10th, we got engaged. We were married on April 29, and found out about Baby Duffey on November 22nd. It was a whirlwind of adventures, and I thank God for giving me such an amazing person in my life. </p>
+      
         </div>
     );
 };
