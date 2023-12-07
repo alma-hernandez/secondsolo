9ddfe6f3a996bd304f36a6bb61d2ba90dbe36025
Alma Duffey <1984ahern@gmail.com>
2023-12-07 12:32:10 -0600

    transitioning images not working on AboutAlma

--- src/components/AboutAlma.js
+++ src/components/AboutAlma.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ColorSchemesExample from './Navbar';
+import "./AboutAlma.css"
+
 
 const AboutAlma = () => {
+    const almaImages = [
+        '../images/boerneLake.jpg',
+        '../images/mattJeffHome.jpg',
+        '../images/thirdDate.JPG'
+    ];
+
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % almaImages.length);
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+
+
     return (
-        < ColorSchemesExample />
+        <div>
+            < ColorSchemesExample />
+            <div>
+                <h1 id="almatitle">About Me</h1>
+            </div>
+            <img
+                className="transition-image large-image"
+                src={almaImages[currentImageIndex]}
+                alt={`Image ${currentImageIndex}`}
+            />
+            <p id="almainfo">I was born March 22,l 1984 at 5:04am, and my mother has woken me up at that time every year on my birthday. I lived in San Antonio until I was nine, then my mother, brother, and I moved to Harlingen to be closer to my mom's side of the family. It was a little too slow-paced for me, so as soon as I turned 18, I came back to San Antonio for college. I didn't particularly care for college so I worked at different restaurants over the years and eventually became a manager. I worked long hours and didn't make time to date seriously. After about 20years of that, I knew I wanted a child, and time was ticking. I was working at a place that gave me more of a normal schedule, so I made a date with Michael. I had to cancel our inital meeting because I did have to work late. Thank goodness he gave me a second chance! </p>
+        </div>
+
+
     );
 };
 
