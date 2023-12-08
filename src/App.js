dc4a1897235bf5af15bd2f3942a21acd6d13dbd7
Alma Duffey <1984ahern@gmail.com>
2023-12-07 23:49:53 -0600

    Pushing code to start fixing site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import './App.css';
+
+// From Michael: If you use export components, you can import all of these from the components folder, which makes this look a little nicer.
 import Home from './components/Home';
 import AboutAlma from './components/AboutAlma';
 import AboutMichael from './components/AboutMichael';
--- src/components/AboutMichael.js
+++ src/components/AboutMichael.js
@@ -9,7 +9,7 @@ const AboutMichael = () => {
         <div id="michaelspage">
         < ColorSchemesExample />
         <h1 id="michaeltitle">About My Amazing Husband</h1>
-        <p id="michaelinfo" >Michael is the youngest of three children, andhis siblings swear he is the favorite child. He was born in Dallas and his parents are Greg and Deb. He went to college without finishing high school, because he was so smart. He has always loved computers and games and made a great career out of it. He was also an actor in college, but I haven't seen videoes of his acting yet. He lived in New York for a while, then moved back to Texas. There's an ex-wife in there somewhere, but moving on. He has owned our home on Delaware in Southtown San Antonio for about ten years. Now he's happily married to the right woman and will remain so until the end of time!</p>
+        <p id="michaelinfo" >Michael is the youngest of three children, and his siblings swear he is the favorite child. He was born in Dallas and his parents are Greg and Deb. He went to college without finishing high school, because he was so smart. He has always loved computers and games and made a great career out of it. He was also an actor in college, but I haven't seen videoes of his acting yet. He lived in New York for a while, then moved back to Texas. There's an ex-wife in there somewhere, but moving on. He has owned our home on Delaware in Southtown San Antonio for about ten years. Now he's happily married to the right woman and will remain so until the end of time!</p>
         <ImageCarousel />
 
         </div>
